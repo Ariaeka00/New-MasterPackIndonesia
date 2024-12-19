3a6dd261289e8a883413c95123432b111227ea71
ariaeka <ariaeka089@gmail.com>
2024-12-19 19:04:30 +0700

    menambahkan list-produk

--- public/js/script.js
+++ public/js/script.js
@@ -1,27 +1,22 @@
-// console.log('Hallo World')
-// $.ajax({
-//     url: "http://192.168.100.26:3000/get_product",
-//     type: "GET",
-//     dataType: "json",
-//     success: function(data) {
-//         console.log(data)
-//         $.each(data, function(i, data) {
-//             $('#daftar-produk').append(`<div class="w-full lg:w-1/2 xl:w-1/3">
-//                     <div class="rounded-xl shadow-lg bg-white overflow-hidden mb-10 dark:bg-slate-800">
-//                         <img src="http://192.168.100.26:3000/image/${data.gambar}" alt="Programming" class="w-full">
-
-//                         <div class="py-8 px-6">
-//                             <h3 class=" mb-3 text-xl font-semibold text-dark truncate dark:text-white">${data.nama}
-//                             </h3>
-//                             <p class="text-secondary mb-4 font-medium text-base">${data.description}</p>
-//                                 <h3 class=" mb-3 text-xl font-semibold text-dark truncate dark:text-white">${data.harga}
-//                                 </h3>
-//                             <a href="#"
-//                                 class="font-medium text-white text-sm bg-red-700 py-2 px-4 rounded-lg hover:opacity-90">Baca
-//                                 Selengkapnya</a>
-//                         </div>
-//                     </div>
-//                 </div>`)
-//         })
-//     }
-// })
+console.log('Hallo World')
+$.ajax({
+    url: "http://192.168.100.26:3000/get_product",
+    type: "GET",
+    dataType: "json",
+    success: function(data) {
+        console.log(data)
+        $.each(data, function(i, data) {
+            $('#list-product').append(`<div class="w-[23rem] flex flex-wrap flex-col border border-aaa shadow-sm bg-slate-100 rounded">
+                    <div class=" mb-4 relative overflow-hidden">
+                        <img src="http://192.168.100.26:3000/image/${data.gambar}" alt="" class="">
+                    </div>
+                    <div class=" w-full pl-3 pr-3 flex flex-wrap flex-col">
+                        <h5 class="w-full font-Poppins font-bold text-xl mb-3 whitespace-nowrap overflow-hidden text-ellipsis">${data.nama}</h5>
+                        <p class="mb-3">${data.description}</p>
+                        <h5 class="font-bold text-lg mb-3">Rp. ${data.harga}</h5>
+                        <a href="" class="mb-3">Beli seakrang</a>
+                    </div>
+                </div>`)
+        })
+    }
+})
